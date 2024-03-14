c55e74f43355405a49ce3c3f24daf84f5a9bc0cd
Rafael Souza da Silva <rafael.souza@logsmart.com.br>
2024-03-13 21:21:59 -0300

    Adicionando React Router Dom e paginas Home e About

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-function App() {
+import {Routes, Route, Link} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+export default function App() {
   return (
-    <div className="App">
-      Olá
-    </div>
+      <div className="App">
+          <nav>
+              <ul id="navigation">
+                  <li>
+                      <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                      <Link to="/about">About</Link>
+                  </li>
+              </ul>
+          </nav>
+          <Routes>
+              <Route exact path="/" element={<Home />}/>
+              <Route path="/about" element={<About />}/>
+          </Routes>
+      </div>
   );
 }
-
-export default App;
